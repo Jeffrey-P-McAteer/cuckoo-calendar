c791102d61cd9dc8a809b07d8bbab8f9e6d7e1c1
Jeffrey McAteer <jeffrey.p.mcateer.civ@mail.mil>
2024-07-23 07:47:26 -0400

    More text-placement logic. 150.0 was experimentally derived

--- cuckoo-calendar.py
+++ cuckoo-calendar.py
@@ -42,7 +42,6 @@ except:
   ])
   import colorthief
 
-
 try:
   import joblib
 except:
@@ -50,6 +49,30 @@ except:
     sys.executable, '-m', 'pip', 'install', f'--target={site_packages}', 'joblib'
   ])
   import joblib
+
+try:
+  import colormath
+  import colormath.color_objects # using  sRGBColor, LabColor
+  import colormath.color_conversions # using  convert_color
+  import colormath.color_diff # using  delta_e_cie2000
+except:
+  subprocess.run([
+    sys.executable, '-m', 'pip', 'install', f'--target={site_packages}', 'colormath'
+  ])
+  import colormath
+  import colormath.color_objects # using  sRGBColor, LabColor
+  import colormath.color_conversions # using  convert_color
+  import colormath.color_diff # using  delta_e_cie2000
+
+try:
+  # colormath needs to update the numpy functions they're using, but for now we monkey-patch numpy.
+  import numpy
+  def patch_asscalar(a):
+      return a.item()
+  setattr(numpy, "asscalar", patch_asscalar)
+except:
+  traceback.print_exc()
+
 
 cache_dir = os.path.join(os.path.dirname(__file__), 'cache')
 os.makedirs(cache_dir, exist_ok=True)
@@ -80,6 +103,19 @@ def get_pallet(path_to_img, color_count=3):
 def invert_color(color):
   r, g, b = color
   return ( max(0, 255 - min(255, r)), max(0, 255 - min(255, g)), max(0, 255 - min(255, b)) )
+
+def color_similarity(color1, color2):
+  return colormath.color_diff.delta_e_cie2000(
+    colormath.color_conversions.convert_color(
+      colormath.color_objects.sRGBColor(color1[0], color1[1], color1[2], is_upscaled=True),
+      colormath.color_objects.LabColor
+    ),
+    colormath.color_conversions.convert_color(
+      colormath.color_objects.sRGBColor(color2[0], color2[1], color2[2], is_upscaled=True),
+      colormath.color_objects.LabColor
+    )
+  )
+
 
 def main(args=sys.argv):
 
@@ -115,9 +151,13 @@ def main(args=sys.argv):
   pdf.set_page_background(month_imgs[0])
   pdf.add_page()
   title_page_color = get_avg_color(month_imgs[0])
-  title_page_pallet = get_avg_color(month_imgs[0])
+  title_page_pallet = get_pallet(month_imgs[0], color_count=4)
   #title_text_color = invert_color(title_page_color)
-  title_text_color = invert_color(random.choice(get_pallet(month_imgs[0], color_count=4)))
+  title_text_color = invert_color(random.choice(title_page_pallet))
+  title_text_outline_possibilities = [ c for c in title_page_pallet if color_similarity(c, title_text_color) > 20.0 ]
+  if len(title_text_outline_possibilities) < 1:
+    title_text_outline_possibilities.append( title_page_color )
+  title_text_outline = random.choice(title_text_outline_possibilities)
 
   year_x = random.uniform(0.9, 6.6)
   year_y = random.uniform(1.3, 6.0)
@@ -125,7 +165,7 @@ def main(args=sys.argv):
   pdf.set_font('helvetica', size=164)
 
   outline_offset = 0.014
-  pdf.set_text_color(title_page_color)
+  pdf.set_text_color(title_text_outline)
   pdf.text(year_x-outline_offset, year_y-outline_offset, text=f'{year}')
   pdf.text(year_x-outline_offset, year_y+outline_offset, text=f'{year}')
   pdf.text(year_x+outline_offset, year_y-outline_offset, text=f'{year}')
@@ -145,24 +185,25 @@ def main(args=sys.argv):
     if dist_to_year > 7.0:
       break
 
-  pdf.set_font('helvetica', size=32)
+  title_subtitle_size_pt = 32
+  pdf.set_font('helvetica', size=title_subtitle_size_pt)
 
-  outline_offset = 0.012
-  cell_w = 0
-  cell_h = 0
-  pdf.set_text_color(title_page_color)
-  pdf.set_x(subtitle_x-outline_offset) ; pdf.set_y(subtitle_y-outline_offset)
-  pdf.cell(cell_w, cell_h, text=f'{title_subtitle}', align=subtitle_alignment)
-  pdf.set_x(subtitle_x-outline_offset) ; pdf.set_y(subtitle_y+outline_offset)
-  pdf.cell(cell_w, cell_h, text=f'{title_subtitle}', align=subtitle_alignment)
-  pdf.set_x(subtitle_x+outline_offset) ; pdf.set_y(subtitle_y-outline_offset)
-  pdf.cell(cell_w, cell_h, text=f'{title_subtitle}', align=subtitle_alignment)
-  pdf.set_x(subtitle_x+outline_offset) ; pdf.set_y(subtitle_y+outline_offset)
-  pdf.cell(cell_w, cell_h, text=f'{title_subtitle}', align=subtitle_alignment)
+
+  title_subtitle_h, title_subtitle_w = pdf.current_font.get_text_width(f'{title_subtitle}', title_subtitle_size_pt, {})
+  subtitle_alinment_offset = 0.0
+  if subtitle_alignment == 'R':
+    subtitle_alinment_offset = -(title_subtitle_w / 150.0) # 300.0 points-per-inch?
+
+  outline_offset = 0.008
+  pdf.set_text_color(title_text_outline)
+  pdf.text((subtitle_x-outline_offset)+subtitle_alinment_offset, subtitle_y-outline_offset, text=f'{title_subtitle}')
+  pdf.text((subtitle_x-outline_offset)+subtitle_alinment_offset, subtitle_y+outline_offset, text=f'{title_subtitle}')
+  pdf.text((subtitle_x+outline_offset)+subtitle_alinment_offset, subtitle_y-outline_offset, text=f'{title_subtitle}')
+  pdf.text((subtitle_x+outline_offset)+subtitle_alinment_offset, subtitle_y+outline_offset, text=f'{title_subtitle}')
 
   pdf.set_text_color(title_text_color)
-  pdf.set_x(subtitle_x) ; pdf.set_y(subtitle_y)
-  pdf.cell(cell_w, cell_h, text=f'{title_subtitle}', align=subtitle_alignment)
+  pdf.text(subtitle_x+subtitle_alinment_offset, subtitle_y, text=f'{title_subtitle}')
+
 
   # End Title Front
 
@@ -174,16 +215,21 @@ def main(args=sys.argv):
     pdf.add_page()
 
     image_page_color = get_avg_color(month_imgs[month_i])
-    image_page_pallet = get_avg_color(month_imgs[month_i])
-    image_text_color = invert_color(image_page_color)
+    image_page_pallet = get_pallet(month_imgs[month_i], color_count=3)
+    #image_text_color = invert_color(image_page_color)
+    image_text_color = random.choice(image_page_pallet)
+    title_text_outline_possibilities = [ c for c in image_page_pallet if color_similarity(c, image_text_color) > 20.0 ]
+    if len(title_text_outline_possibilities) < 1:
+      title_text_outline_possibilities.append( title_page_color )
+    title_text_outline = random.choice(title_text_outline_possibilities)
 
-    month_x = random.uniform(0.9, 8.0)
+    month_x = random.uniform(0.9, 7.0)
     month_y = random.uniform(0.9, 6.0)
 
     pdf.set_font('helvetica', size=78)
 
     outline_offset = 0.009
-    pdf.set_text_color(image_page_color)
+    pdf.set_text_color(title_text_outline)
     pdf.text(month_x-outline_offset, month_y-outline_offset, text=f'{month_name}')
     pdf.text(month_x-outline_offset, month_y+outline_offset, text=f'{month_name}')
     pdf.text(month_x+outline_offset, month_y-outline_offset, text=f'{month_name}')
@@ -199,6 +245,9 @@ def main(args=sys.argv):
     pdf.set_font('helvetica', size=14)
     pdf.set_text_color((6, 6, 6))
     pdf.cell(text=f'S/M/Tu/W/Th/F/Sat, avg color = {get_avg_color(month_imgs[month_i])}\npallet = {get_pallet(month_imgs[month_i])}')
+    pdf.ln()
+    pdf.cell(text=f'color_similarity(p, image_page_color) = [{[color_similarity(p, image_page_color) for p in image_page_pallet]}]')
+    pdf.ln()
 
   # Finally, put this config on final page
   pdf.set_page_background(None)
